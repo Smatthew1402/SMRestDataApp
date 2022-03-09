cd04d04d6c10d011986cab5802d6b5cf46f5e832
Smatthew1402 <100808047+Smatthew1402@users.noreply.github.com>
2022-03-09 11:29:25 -0500

    Removed extraneous main class

--- ReadImage.java
+++ ReadImage.java
@@ -8,22 +8,7 @@ import javax.swing.JLabel;
 
 public class ReadImage 
 {    
-    public static void main( String[] args )
-    {
-        Image image = null;
-        try {
-            URL url = new URL("https://files.bikeindex.org/uploads/Pu/494423/small_IMG_20200830_150015.jpg");
-            image = ImageIO.read(url);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        
-        JFrame frame = new JFrame();
-        //frame.setSize(300, 300);
-        JLabel label = new JLabel(new ImageIcon(image));
-        frame.add(label);
-        frame.setVisible(true);
-    }
+    
     public static void displayImage(String URL){
         Image image=null;
         try {
